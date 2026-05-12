ca6e4fdae6de485af900c573a5ce123b246ef0eb
metalxin <goldxin1024@gmail.com>
2026-05-12 23:32:42 +0800

    fix(route): enlarge map display area
    - Increase map container height from 480rpx to 640rpx
    - Adjust detail panel height to match new map size
    - Reduce blank space at bottom of page

--- qutu-uni/src/pages/route/detail.vue
+++ qutu-uni/src/pages/route/detail.vue
@@ -544,7 +544,7 @@ $accent: #FF6D00;
 /* 地图区域 */
 .map-container {
   width: 100%;
-  height: 480rpx;
+  height: 640rpx;
   position: relative;
 }
 
@@ -634,7 +634,7 @@ $accent: #FF6D00;
 
 /* 详情面板 */
 .detail-panel {
-  height: calc(100vh - 480rpx);
+  height: calc(100vh - 640rpx);
   padding: 0 32rpx;
   box-sizing: border-box;
 }
